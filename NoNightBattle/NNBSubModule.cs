2dfff9ad94b47bba50dc1de8fd8d55a3446a0b91
yichuan.jiang <46229320@qq.com>
2024-03-24 18:57:04 +0800

    First version

---
+++ NoNightBattle/NNBSubModule.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Windows.Forms;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace NoNightBattle
+{
+    public class NNBSubModule : MBSubModuleBase
+    {
+        private static readonly string Namespace = typeof(NNBSubModule).Namespace;
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony(Namespace).PatchAll();
+        }
+    }
+}
---
+++ NoNightBattle/Patches/NoNightBattlePatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+
+namespace NoNightBattle.Patches
+{
+    [HarmonyPatch(typeof(DefaultMapWeatherModel), "GetHourOfDay")]
+    internal class NoNightBattlePatch
+    {
+        [HarmonyPostfix]
+        public static void GetHourOfDay(DefaultMapWeatherModel __instance,ref float __result)
+        {
+            if (__result <= 6.0f)
+            {
+                __result = 6f;
+            }
+            else if (__result >=18.0f)
+            {
+                __result = 18.0f;
+            }
+        }
+    }
+}
